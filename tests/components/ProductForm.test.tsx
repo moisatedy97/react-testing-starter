3cd9998d3288c15da4078c9291fd6dc16580a040
gmoisa <gabriel.moisa@wareplace.com>
2024-07-01 18:02:07 +0200

    Finished lesson 53 part2.

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import ProductForm from "../../src/components/ProductForm";
 import AllProviders from "../all-providers";
 
@@ -6,6 +6,9 @@ describe("ProductForm", () => {
   it("should render form fields", async () => {
     render(<ProductForm onSubmit={vi.fn()} />, { wrapper: AllProviders });
 
-    expect(await screen.findByRole("textbox", { name: /name/i })).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+
+    expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/price/i)).toBeInTheDocument();
   });
 });
